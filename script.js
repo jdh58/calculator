e075ca19ea023ef9d8ee648a9d82a29727e74bea
Jonathan Hawes <jhawes580@gmail.com>
2022-08-09 14:18:51 -0700

    Reapir calculator to compare two numbers at once Calculator is now fully functional (I believe)

--- script.js
+++ script.js
@@ -6,43 +6,72 @@
     return sumAll;
 }*/
 
-add = (a, b) => a + b;
+add = (a, b) => parseFloat(a) + parseFloat(b);
 sub = (a, b) => a - b;
 mult = (a, b) => a * b;
 div = (a, b) => a / b;
 
 function operate(operator, a, b) {
+    console.log('dsa');
     if (operator == '+') { return add(a, b); }
     if (operator == '-') { return sub(a, b); }
     if (operator == '*') { return mult(a, b); }
     if (operator == '/') { return div(a, b); }
 }
 
-const buttons = document.querySelectorAll('.buttons');
+const numButtons = document.querySelectorAll('.num');
+const opButtons = document.querySelectorAll('.operator')
 const screenText = document.getElementById('screen-text');
+const equalsButton = document.getElementById('equals');
+const clearButton = document.getElementById('clear');
+let first = '';
+let operator = '';
+let second = '';
 
-buttons.forEach(button => button.addEventListener('click', runPressed));
+numButtons.forEach(button => button.addEventListener('click', numPressed));
+opButtons.forEach(button => button.addEventListener('click', opPressed));
+equalsButton.addEventListener('click', equalsPressed);
+clearButton.addEventListener('click', clearPressed);
 
-function runPressed(e) {
-    // Declare text as an empty string
-    let text = '';
+function numPressed(e) {
+    first += getId(e);
+    screenText.textContent = first;
+}
 
+function opPressed(e) {
+    numButtons.forEach(button => button.removeEventListener('click', numPressed));
+    numButtons.forEach(button => button.addEventListener('click', secondPressed));
+    operator = getId(e);
+    console.log(first, operator);
+}
+
+function secondPressed(e) {
+    second += getId(e);
+    screenText.textContent = second;
+}
+
+function equalsPressed() {
+    screenText.textContent = operate(operator, first, second);
+    first = screenText.textContent;
+    second = '';
+    operator = '';
+}
+
+function getId(e) {
     /* If the user clicked the nested element, set the text equal to
     the button's id. If they pressed the button, do the same. */
     if (e.target.nodeName == 'P') {
-        text = e.composedPath()[1].id;
+        return e.composedPath()[1].id;
     } else {
-        text = e.target.id;
+        return e.target.id;
     }
+}
 
-    screenText.textContent += text;
-
-    // If the user pressed clear, wipe the screen
-    if (text === 'clear') {
-        screenText.textContent = '';
-    }
-
-    if (text === '=') {
-
-    }
+function clearPressed() {
+    numButtons.forEach(button => button.removeEventListener('click', secondPressed));
+    numButtons.forEach(button => button.addEventListener('click', numPressed));
+    first = '';
+    second = '';
+    operator = '';
+    screenText.textContent = '';
 }
